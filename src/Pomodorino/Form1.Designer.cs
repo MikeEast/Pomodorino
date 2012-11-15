9442fc2ef42a80d4cb70de577b8c4c89d63fad36
Mikael Ostberg <mikael@inloop.se>
2012-11-15 21:48:14 +0100

    Initial commit

---
+++ src/Pomodorino/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Pomodorino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timeDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 101);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(197, 101);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Exit";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timeDisplay
+            // 
+            this.timeDisplay.BackColor = System.Drawing.Color.Black;
+            this.timeDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.timeDisplay.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeDisplay.ForeColor = System.Drawing.Color.Lime;
+            this.timeDisplay.Location = new System.Drawing.Point(13, 13);
+            this.timeDisplay.Name = "timeDisplay";
+            this.timeDisplay.Size = new System.Drawing.Size(259, 59);
+            this.timeDisplay.TabIndex = 2;
+            this.timeDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 136);
+            this.Controls.Add(this.timeDisplay);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pomodorino";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox timeDisplay;
+    }
+}
+
---
+++ src/Pomodorino/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pomodorino
+{
+    public partial class Form1 : Form
+    {
+        AppState appState;
+        DateTime endTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            appState = AppState.Initial;
+            timer.Interval = 1000;
+            timer.Tick += timer_Tick;
+            UpdateTexts();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            endTime = DateTime.Now.AddMinutes(20);
+            timer.Start();
+            appState = AppState.Running;
+            UpdateTexts();
+        }
+
+        void UpdateTexts()
+        {
+            switch (appState)
+            {
+                case AppState.Initial:
+                case AppState.Stopped:
+                    timeDisplay.Text = "20:00";
+                    stopButton.Text = "Exit";
+                    break;
+                case AppState.Running:
+                    stopButton.Text = "Stop";
+                    break;
+                case AppState.Alarming:
+                    timeDisplay.Text = "Done!";
+                    stopButton.Text = "Stop";
+                    break;
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            switch (appState)
+            {
+                case AppState.Running:
+                    var remainingTime = (DateTime.Now - endTime);
+                    timeDisplay.Text = remainingTime.ToString("mm\\:ss");    
+                    if (remainingTime.TotalSeconds >= 0)
+                    {
+                        appState = AppState.Alarming;
+                        UpdateTexts();
+                    }
+                    break;
+                case AppState.Alarming:
+                    System.Media.SystemSounds.Beep.Play();
+                    break;
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            switch (appState)
+            {
+                case AppState.Initial:
+                case AppState.Stopped:
+                    Application.Exit();
+                    break;
+                case AppState.Running:
+                case AppState.Alarming:
+                    timer.Stop();
+                    appState = AppState.Stopped;
+                    break;
+            }
+            UpdateTexts();
+        }
+    }
+
+    internal enum AppState
+    {
+        Initial,
+        Running,
+        Alarming,
+        Stopped
+    }
+}
